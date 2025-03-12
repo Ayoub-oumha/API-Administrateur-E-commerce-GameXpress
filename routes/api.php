9ac1bb3ed2618912facb14752184cb3e0756eaff
ayoub oumha <ayyouboumha@gmail.com>
2025-03-12 02:55:33 +0000

    add role and permission middleware, update database seeder for user roles, and enhance API routes

--- routes/api.php
+++ routes/api.php
@@ -6,19 +6,26 @@ use App\Http\Controllers\Api\RoleController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-
+// Auth routes
 Route::get('/sayHello', [AuthController::class, 'sayHello']);
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
 Route::middleware('auth:sanctum')->group(function() {
     Route::post('/logout', [AuthController::class, 'logout']);
 });
-
-
-Route::middleware('role:super_admin')->prefix('admin')->group(function() {
-    // Gestion des rôles
-    Route::get('/roles', [RoleController::class, 'index']);
+// Admin routes
+Route::middleware('auth:sanctum')->group(function() {
+    Route::middleware('role:super_admin')->prefix('admin')->group(function() {
+    
+        Route::get('/roles', [RoleController::class, 'index']);
+       
 });
+});
+
+
+
+// test route
 Route::get('/test', function() {
     return response()->json(['message' => 'Test route works!']);
+   
 });
